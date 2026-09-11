1eeda4ca64b8c340d8d8415769cd7f9a8cbb42c7
230040@utxicotepec.edu.mx <>
2026-09-11 16:22:52 -0600

    3 commit

--- hello_world_app_230040/lib/screens/counters/counter_screen.dart
+++ hello_world_app_230040/lib/screens/counters/counter_screen.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
   @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter Screen')),
+
       body: Center(
-        child: Text('Counter Screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+
+             Text('Click${ clickCounter == 1 ? '':'s'}', style: TextStyle(fontSize: 30)),
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+            setState(() {});
+          });
+        },
+        child: const Icon(Icons.plus_one),
       ),
     );
   }
